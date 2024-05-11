acf200b52d9898029c244e4e6628e6fc32ba2dd8
ngocgiauu <giauntn21411ca@st.uel.edu.vn>
2024-05-11 16:22:15 +0700

    1

---
+++ rehome web/my-server-mongodb/Index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const port = 3002;
+
+const morgan=require("morgan")
+app.use(morgan("combined"))
+
+const bodyParser=require("body-parser")
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+const cors=require("cors");
+app.use(cors())
+
+app.listen(port,()=>{
+    console.log(`My Server listening on port ${port}`)
+})
+
+app.get("/",(req,res)=>{
+    res.send("This Web server is processed for MongoDB for rehome database")
+})
+
+const { MongoClient, ObjectId } = require('mongodb');
+client = new MongoClient("mongodb://127.0.0.1:27017");
+client.connect();
+database = client.db("rehome"); 
+productCollection = database.collection("Product");
+
+app.get("/product",cors(),async (req,res)=>{ 
+    const result = await productCollection.find({}).toArray();
+    res.send(result)
+    }
+    )
+    
